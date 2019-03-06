a35294fe71214ba66068cd507c80c1b8d6746f0c
FelipeBarros <barrosfelipe.013@gmail.com>
2019-03-06 20:07:33 -0300

    Add componente Produto e Expansão dele

---
+++ stbl/src/components/Produto/Produto.js
@@ -0,0 +1,83 @@
+import React, { Component } from 'react'
+
+export default class Produto extends Component {
+    constructor() {
+        super();
+        this.state = {
+            contadorQtd: 1
+        }
+
+        this.CliqueVerDetalhes = this.CliqueVerDetalhes.bind(this);
+        this.decrementarQtd = this.decrementarQtd.bind(this);
+        this.incrementarQtd = this.incrementarQtd.bind(this);
+    }
+
+    CliqueVerDetalhes = () => {
+        const modal = document.getElementById(this.props.id);
+        modal.classList.add('mostrar');
+        modal.addEventListener('click', (e) => {
+            if (e.target.id == this.props.id || e.target.className == 'fechar') {
+                modal.classList.remove('mostrar');
+            }
+        })
+    }
+
+    decrementarQtd = () => {
+        if(this.state.contadorQtd == 1){
+            return;
+        }
+        this.setState({
+            contadorQtd: this.state.contadorQtd - 1,
+        });
+    }
+
+    incrementarQtd = () => {
+        this.setState({
+            contadorQtd: this.state.contadorQtd + 1,
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <div>
+                    <div>
+                        <img src={this.props.img} alt="img" className="imagemProduto" />
+                        <button onClick={this.CliqueVerDetalhes} className="botaoVerDetalhes" id="botaoVerDetalhes">Ver detalhes</button>
+                    </div>
+                    <div>
+                        <p>{this.props.marca}</p>
+                        <p>{this.props.titulo}</p>
+                        <p>R${this.props.preco}</p>
+                    </div>
+                </div>
+                <div id={this.props.id} className="modal-container">
+                    <div className="modal">
+                        <div>
+                            <img src={this.props.img} alt="img" className="imagemProduto" />
+                        </div>
+                        <div>
+                            <button className="fechar">X</button>
+                            <h1>{this.props.titulo}</h1>
+                            <p>Disponibilidade: {this.props.estoque}</p>
+                            <p>Tipo de Produto: {this.props.tipoProduto}</p>
+                            <p>Marca: {this.props.marca}</p>
+                            <p>{this.props.descricao}</p>
+                            <p>Escolha o tamanho:</p>
+                            <button>PP</button><button>P</button><button>M</button><button>G</button><button>GG</button>
+                            <h1>R${this.props.preco}</h1>
+                            <div>
+                                <p>Quantidade: </p>
+                                <button onClick={this.decrementarQtd}>-</button>
+                                {this.state.contadorQtd}
+                                <button onClick={this.incrementarQtd}>+</button>
+                            </div>
+                            <p>SubTotal: R${this.props.preco * this.state.contadorQtd}</p>
+                            <button>Adicionar ao Carrinho</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
---
+++ stbl/src/components/Produto/TesteProduto.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+import Produto from "./Produto"
+import deadpool from "./imgs/deadpool.jpg"
+import flash from "./imgs/flash.jpg"
+import supergirl from "./imgs/supergirl.jpg"
+
+
+export default class TesteProduto extends Component {
+    render() {
+        return (
+            <div>
+                <div>
+                    {/*<Produto id={id} img={img} titulo={titulo} estoque={estoque} tipoProduto={tipoProduto}
+          marca={marca} preco={preco} descricao={descricao} tamanho={tamanho} quantidade={quantidade}
+    noCarrinho={noCarrinho}/>*/}
+                    <Produto id={"id1"} img={deadpool} titulo={"titulo"}
+                        marca={"marca"} preco={27.99} estoque={"Em estoque"} tipoProduto={"Camisa Masculina"} descricao={"descricaoooooo"} />
+                    <Produto id={"id2"} img={flash} titulo={"titulo2"}
+                        marca={"marca2"} preco={227.99} estoque={"Em estoque2"} tipoProduto={"Camisa Masculina2"} descricao={"descricaoooooo2"} />
+                    <Produto id={"id3"} img={supergirl} titulo={"titulo3"}
+                        marca={"marca3"} preco={237.99} estoque={"Em estoque3"} tipoProduto={"Camisa Masculina3"} descricao={"descricaoooooo3"} />
+
+                </div>
+            </div>
+        )
+    }
+}
