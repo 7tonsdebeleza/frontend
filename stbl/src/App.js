17683c76cb8c16f42824a49e18ac27aa3a0a3d24
GustavoM7 <gustavosmarquesf@gmail.com>
2019-03-08 19:14:06 -0300

    config style branch

--- stbl/src/App.js
+++ stbl/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Roteador from './components/Roteador/Roteador';
+import 'bootstrap/dist/css/bootstrap.min.css'
 import "./App.css"
 
 class App extends Component {
