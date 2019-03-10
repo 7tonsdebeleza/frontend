01b417bf8a4a8e6e444812dea2f5efd4862b73f1
GustavoM7 <gustavosmarquesf@gmail.com>
2019-03-09 21:18:07 -0300

    Home grid and layout

--- stbl/src/components/Home/Home.js
+++ stbl/src/components/Home/Home.js
@@ -1,70 +1,177 @@
 import React from 'react';
 
-//Estrutura inicial para página de Home;
-
 const Home = () => {
     return(
         <div>
-            <div>
-                {/*Div para banner/carroussel;*/}
-                <h1>LOREM A COSMO</h1>
-                <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h2>
 
-                {/*
-                    Botão dentro do banner (sem função)
-                    No template leva a loja virtual;
-                */}
-                <button>SHOP THE COLLECTION</button>
-            </div>
+            {/*Banner com background
+                futuramente transformar em carrossel (bootstrap não roda)
+            */}
 
-            <div>
-                {/*Div para primeiro card*/}
-                <h2>EDITOR'S PICK // SHOES // ACCESSORIES</h2>
-            </div>
+            <div className='card'>
+                <img class="card-img" style={{height:520}} src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDlFD938Z68Amoo8tuv7RG6ooYcm4aVragpD6uaMRoir-PgPxx' alt="Card image"/>
+                    <div class="card-img-overlay">
+                        <div className='text-center' style={{marginTop: 180}}>
+                            <h1 className='card-title'>LOREM A COSMO</h1>
+                            <p style={{width: '40%'}} className='mx-auto card-text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
 
-            <div>
-                {/*Div para segundo card com breve lista de produtos*/}
-                <h2>---NEW ARRIVALS---</h2>
-                [PRODUTOS]
-            </div>
+                            {/*
+                                Botão dentro do banner (sem função)
+                                No template leva a loja virtual;
+                            */}
 
-            <div>
-                {/*Div para terceiro card*/}
-                <h2>MILANCELOS</h2>
-                <div>Pellentesque posuere orci lobortis scelerisque blandit. Donec id tellus lacinia an, tincidunt risus ac, consequat velit. Quisquemos sodales suscipit tortor ditaemcos condimentum lacus meleifend menean viverra auctor blanditos comodous.</div>
-            </div>
-
-            <div>
-                {/*Div para quarto card com breve lista de produtos*/}
-                <h2>---TRENDING NOW---</h2>
-                [PRODUTOS]
-            </div>
-
-            <div>
-                {/*Div para quinto card*/}
-                <h2>---#FEATURED ON ELLA---</h2>
-                <img src="#"/>
-                <h3>LOREM ET DORUS</h3>
-                <div>Nullam aliquet vestibulum augue non varius. Cras nec congue elitos. Duis tristique del ante nec aliquam. Praesent urna tellus laoreet an tellus ullamcorper fermentum facilisis eratum.</div>
-                <button>SHOP NOW</button>
-
-                <img src="#"/>
-                <h3>MILANCELOS A LANOS</h3>
-                <div>Nullam aliquet vestibulum augue non varius. Cras nec congue elitos. Duis tristique del ante nec aliquam. Praesent urna tellus laoreet an tellus ullamcorper fermentum facilisis eratum.</div>
-                <button>SHOP NOW</button>
-
-                <img src="#"/>
-                <h3>COMOS DE MILANO</h3>
-                <div>Nullam aliquet vestibulum augue non varius. Cras nec congue elitos. Duis tristique del ante nec aliquam. Praesent urna tellus laoreet an tellus ullamcorper fermentum facilisis eratum.</div>
-                <button>SHOP NOW</button>
+                            <button className='btn btn-dark'>SHOP THE COLLECTION</button>
+                        </div>
+                    </div>
             </div>
             
-            <div>
-                {/*Ùltimo card*/}
-                <h2>ELLA ON INSTAGRAM</h2>
-                [API]
-                <button>VIEW GALLERY</button>
+            {/*Div para primeiro card*/}
+            <div className='container justify-content-between p-4'>
+                {/*Divisão em 3 cards, texto sobreposto em background*/}
+
+                <div className='card-deck'>
+                    
+                    <div className='card border-light bg-dark text-white text-center'>
+
+                        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDlFD938Z68Amoo8tuv7RG6ooYcm4aVragpD6uaMRoir-PgPxx'/>
+
+                        <div className='card-img-overlay'>
+                            <h2>EDITOR'S PICK</h2>
+                        </div>
+                    </div>
+
+                    <div className='card border-light bg-dark text-center'>
+
+                        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDlFD938Z68Amoo8tuv7RG6ooYcm4aVragpD6uaMRoir-PgPxx'/>
+
+                        <div className='card-img-overlay'>
+                            <h2>SHOES</h2>
+                        </div>
+                    </div>
+
+                    <div className='card border-light bg-dark text-white text-center'>
+
+                        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDlFD938Z68Amoo8tuv7RG6ooYcm4aVragpD6uaMRoir-PgPxx'/>
+
+                        <div className='card-img-overlay'>
+                            <h2>ACCESSORIES</h2>
+                        </div>
+                    </div>
+
+                </div>
             </div>
+
+
+            <div className='container'>
+                {/*Div para segundo card com breve lista de produtos*/}
+                <div className='p-4 d-flex flex-row justify-content-center align-items-center text-center d-flex'>
+                    <div className='col'><hr/></div>
+                    <div className='col-md-auto'>NEW ARRIVALS</div>
+                    <div className='col'><hr/></div>
+                </div>
+                
+                [PRODUTOS]
+            </div>
+
+
+            <div className='card'>
+                <img class="card-img" style={{height:520}} src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDlFD938Z68Amoo8tuv7RG6ooYcm4aVragpD6uaMRoir-PgPxx' alt="Card image"/>
+                    <div class="card-img-overlay">
+
+                        <div className='row justify-content-center text-center' style={{marginTop: '10%'}}>
+                            <div className='col' style={{width:'100%'}}>{/*Coluna vazia para por paragrafo abaixo alinhado a direita*/}  </div>
+                            <div className='col'>
+                                <h2>MILANCELOS</h2>
+                                <p style={{width: '60%'}} className='mx-auto'>Pellentesque posuere orci lobortis scelerisque blandit. Donec id tellus lacinia an, tincidunt risus ac, consequat velit. Quisquemos sodales suscipit tortor ditaemcos condimentum lacus meleifend menean viverra auctor blanditos comodous.</p>
+
+                                {/*
+                                    Botão dentro do banner (sem função)
+                                    No template leva a loja virtual;
+                                */}
+                                <button className='btn btn-dark'>SHOP THE COLLECTION</button>
+                            </div>
+                        </div>
+
+                    
+                        
+                    </div>
+            </div>
+
+            <div className='container'>
+                {/*Div para quarto card com breve lista de produtos*/}
+                <div className='p-4 d-flex flex-row justify-content-center align-items-center text-center d-flex'>
+                    <div className='col'><hr/></div>
+                    <div className='col-md-auto'>TRENDING NOW</div>
+                    <div className='col'><hr/></div>
+                </div>
+                
+                [PRODUTOS]
+            </div>
+
+            <div className='container'>
+                {/*Div para quinto card*/}
+                <div className='p-4 d-flex flex-row justify-content-center align-items-center text-center d-flex'>
+                    <div className='col'><hr/></div>
+                    <div className='col-md-auto'>#FEATURED ON ELLA</div>
+                    <div className='col'><hr/></div>
+                </div>
+
+                <div className='card-deck text-center'>
+                    <div className='card border-0'>
+                        <img className='card-img-top' src="#"/>
+                            
+                        <div className='card-body'>
+                            <h3 className='card-title'>LOREM ET DORUS</h3>
+                            <p className='card-text'>Nullam aliquet vestibulum augue non varius. Cras nec congue elitos. Duis tristique del ante nec aliquam. Praesent urna tellus laoreet an tellus ullamcorper fermentum facilisis eratum.</p>
+                            <button className='btn btn-dark'>SHOP NOW</button>
+                        </div>
+
+                    </div>
+
+                    <div className='card border-0'>
+                        <img className='card-img-top' src="#"/>
+                            
+                        <div className='card-body'>
+                            <h3 className='card-title'>MILANCELOS A LANOS</h3>
+                            <p className='card-text'>Nullam aliquet vestibulum augue non varius. Cras nec congue elitos. Duis tristique del ante nec aliquam. Praesent urna tellus laoreet an tellus ullamcorper fermentum facilisis eratum.</p>
+                            <button className='btn btn-dark'>SHOP NOW</button>
+                        </div>
+                    </div>
+
+                    <div className='card border-0'>
+
+                        <img className='card-img-top' src="#"/>
+                                
+                        <div className='card-body'>
+                            <h3 className='card-title'>COMOS DE MILANO</h3>
+                            <p className='card-text'>Nullam aliquet vestibulum augue non varius. Cras nec congue elitos. Duis tristique del ante nec aliquam. Praesent urna tellus laoreet an tellus ullamcorper fermentum facilisis eratum.</p>
+                            <button className='btn btn-dark'>SHOP NOW</button>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+
+            <div className='container'>
+                <hr/>
+
+                [BRANDS]
+
+            </div>
+
+            <div style={{backgroundColor:'#FFF5EE'}}>
+                <div className='container text-center'>
+                    {/*Ùltimo card*/}
+                    <h2>#ELLA ON INSTAGRAM</h2>
+                    
+                    <div>
+                        [IMGS]
+                    </div>
+                    <button className='btn btn-dark'>VIEW GALLERY</button>
+                </div>
+            </div>
+            
 
         </div>
     )
