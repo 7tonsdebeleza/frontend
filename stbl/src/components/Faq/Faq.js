d74ba28ead7eac9a7a25743ad0418004d6557f74
GustavoM7 <gustavosmarquesf@gmail.com>
2019-03-23 18:41:33 -0300

    concluindo faq

--- stbl/src/components/Faq/Faq.js
+++ stbl/src/components/Faq/Faq.js
@@ -6,17 +6,126 @@ const Faq = () => {
         <div className="container">
             <h3 className="spotlight" style={tituloPeriferico}>Perguntas Frequentes </h3>
             <hr/>
-            <p className="text-center discreet-arimo">
+            <p className="text-center discreet-arimo" style={textoPeriferico}>
                 Abaixo em nosso FAQ estão algumas preocupações comuns de nossos clientes antes de comprar um produto, se você tiver outras perguntas, por favor, basta enviá-lo para support@email.com.
             </p>
 
+            {/*Abaixo, lista com perguntas estáticas em lorem ipsum
+                escalável para renderização dinâmica com bd pelo componente <Question/> e suas props
+                , caso precise.
+            */}
+
             <p>
                 <Question dropped = {true} texts = {{
-                        quest: "Quest test",
-                        answer: "Answer test"
+                        quest: "Nascetur ridiculus mus mauris vitae ultricies leo integer?",
+                        answer: "Turpis egestas pretium aenean pharetra magna ac placerat vestibulum. Erat velit scelerisque in dictum non. Dolor sit amet consectetur adipiscing"
                     }
                 }/>
-                
+            </p>
+            <p>
+                <Question dropped = {false} texts = {{
+                        quest: "Dolor morbi non arcu risus quis varius quam quisque. Id porta nibh venenatis cras sed felis eget velit aliquet?",
+                        answer: "Turpis egestas pretium aenean pharetra magna ac placerat vestibulum. Erat velit scelerisque in dictum non. Dolor sit amet consectetur adipiscing"
+                    }
+                }/>
+            </p>
+            <p>
+                <Question dropped = {false} texts = {{
+                        quest: "Imperdiet dui accumsan sit amet nulla facilisi morbi tempus?",
+                        answer: "Turpis egestas pretium aenean pharetra magna ac placerat vestibulum. Erat velit scelerisque in dictum non. Dolor sit amet consectetur adipiscing"
+                    }
+                }/>
+            </p>
+            <p>
+                <Question dropped = {false} texts = {{
+                        quest: "Massa enim nec dui nunc?",
+                        answer: "Turpis egestas pretium aenean pharetra magna ac placerat vestibulum. Erat velit scelerisque in dictum non. Dolor sit amet consectetur adipiscing"
+                    }
+                }/>
+            </p>
+            <p>
+                <Question dropped = {false} texts = {{
+                        quest: "Vestibulum lectus mauris ultrices eros in cursus turpis massa?",
+                        answer: "Turpis egestas pretium aenean pharetra magna ac placerat vestibulum. Erat velit scelerisque in dictum non. Dolor sit amet consectetur adipiscing"
+                    }
+                }/>
+            </p>
+            <p>
+                <Question dropped = {false} texts = {{
+                        quest: "Nascetur ridiculus mus mauris vitae ultricies leo integer?",
+                        answer: "Turpis egestas pretium aenean pharetra magna ac placerat vestibulum. Erat velit scelerisque in dictum non. Dolor sit amet consectetur adipiscing"
+                    }
+                }/>
+            </p>
+            <p>
+                <Question dropped = {false} texts = {{
+                        quest: "Viverra maecenas accumsan lacus vel facilisis volutpat est velit egestas?",
+                        answer: "Turpis egestas pretium aenean pharetra magna ac placerat vestibulum. Erat velit scelerisque in dictum non. Dolor sit amet consectetur adipiscing"
+                    }
+                }/>
+            </p>
+            <p>
+                <Question dropped = {false} texts = {{
+                        quest: "Nascetur ridiculus mus mauris vitae ultricies leo integer?",
+                        answer: "Turpis egestas pretium aenean pharetra magna ac placerat vestibulum. Erat velit scelerisque in dictum non. Dolor sit amet consectetur adipiscing"
+                    }
+                }/>
+            </p>
+            <p>
+                <Question dropped = {false} texts = {{
+                        quest: "Netus et malesuada fames ac turpis egestas maecenas pharetra?",
+                        answer: "Turpis egestas pretium aenean pharetra magna ac placerat vestibulum. Erat velit scelerisque in dictum non. Dolor sit amet consectetur adipiscing"
+                    }
+                }/>
+            </p>
+            <p>
+                <Question dropped = {false} texts = {{
+                        quest: "Pellentesque habitant morbi tristique senectus et netus et?",
+                        answer: "Turpis egestas pretium aenean pharetra magna ac placerat vestibulum. Erat velit scelerisque in dictum non. Dolor sit amet consectetur adipiscing"
+                    }
+                }/>
+            </p>
+            <p>
+                <Question dropped = {false} texts = {{
+                        quest: "Nascetur ridiculus mus mauris vitae ultricies leo integer?",
+                        answer: "Turpis egestas pretium aenean pharetra magna ac placerat vestibulum. Erat velit scelerisque in dictum non. Dolor sit amet consectetur adipiscing"
+                    }
+                }/>
+            </p>
+            <p>
+                <Question dropped = {false} texts = {{
+                        quest: "Vulputate mi sit amet mauris commodo quis imperdiet massa. Egestas integer eget aliquet nibh praesent tristique?",
+                        answer: "Turpis egestas pretium aenean pharetra magna ac placerat vestibulum. Erat velit scelerisque in dictum non. Dolor sit amet consectetur adipiscing"
+                    }
+                }/>
+            </p>
+            <p>
+                <Question dropped = {false} texts = {{
+                        quest: "Nascetur ridiculus mus mauris vitae ultricies leo integer?",
+                        answer: "Turpis egestas pretium aenean pharetra magna ac placerat vestibulum. Erat velit scelerisque in dictum non. Dolor sit amet consectetur adipiscing"
+                    }
+                }/>
+            </p>
+            <p>
+                <Question dropped = {false} texts = {{
+                        quest: "Malesuada fames ac turpis egestas sed tempus urna et. Viverra tellus in hac habitasse platea dictumst vestibulum rhoncus. Magna eget est lorem ipsum dolor sit?",
+                        answer: "Turpis egestas pretium aenean pharetra magna ac placerat vestibulum. Erat velit scelerisque in dictum non. Dolor sit amet consectetur adipiscing"
+                    }
+                }/>
+            </p>
+            <p>
+                <Question dropped = {false} texts = {{
+                        quest: "Nascetur ridiculus mus mauris vitae ultricies leo integer?",
+                        answer: "Turpis egestas pretium aenean pharetra magna ac placerat vestibulum. Erat velit scelerisque in dictum non. Dolor sit amet consectetur adipiscing"
+                    }
+                }/>
+            </p>
+            <p>
+                <Question dropped = {false} texts = {{
+                        quest: "Imperdiet massa tincidunt nunc pulvinar sapien et ligula ullamcorper malesuada?",
+                        answer: "Turpis egestas pretium aenean pharetra magna ac placerat vestibulum. Erat velit scelerisque in dictum non. Dolor sit amet consectetur adipiscing"
+                    }
+                }/>
             </p>
 
         </div>
@@ -29,4 +138,11 @@ export default Faq;
 const tituloPeriferico = {
     fontSize: '20px',
     marginTop: '10px'
+}
+
+const textoPeriferico = {
+    maxWidth: '60%',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    fontSize: '14px',
 }
--- stbl/src/components/Faq/Question.js
+++ stbl/src/components/Faq/Question.js
@@ -1,44 +1,91 @@
 import React, { Component } from 'react';
+import './QuestionStyle.css';
 
 class Question extends Component {
     state = {
-        dropped: this.props.dropped
+        dropped: this.props.dropped //Estado para determinar se resposta é visível ou não, recebe valor default na props
     }
 
     show = () =>{
+        //Função para alternar estado da visibilidade da resposta
         this.setState({
             dropped: this.state.dropped ? false : true,
         })
     }
 
     render(){
-        let answer;
+        let answer; //Receberá props de resposta se estado estiver visível, receberá null caso contrário
+        let textAction; //Texto complementar (mostrar ou esconder)
+        let dinamicBg; //Variável para controle do background dinâmico
+        let animation; //Variável para controle da animação de sugir resposta
 
         if(this.state.dropped){
-            answer = (<div><p>{this.props.texts.answer}</p></div>)
+            //State true, answer recebe props e estrutura para jsx
+            answer = (
+                <div className="d-flex justify-content-between btnQuestionText">
+                        <div className="">
+                            <p className="badge badge-success">Resposta</p>
+                        </div>
+                        <div className="questionText">
+                            <p>
+                                {this.props.texts.answer}
+                            </p>
+                        </div>
+                </div>);
+
+            //Texto muda para "esconder"
+            textAction = "Esconder";
+
+            //Background muda para tema dark
+            dinamicBg = {
+                color: '#f8f8f7',
+                backgroundColor: 'rgb(52, 58, 64)'
+            };
+
+            //Animação surgindo
+            animation = {
+                transform: 'scaleY(1)',
+            };
+
         } else {
-            answer = (null)
+
+            //State false, answer vazia
+            answer = (null);
+
+            //Texto muda para "mostrar"
+            textAction = "Mostrar";
+
+            //Desfazendo animação
+            animation = {
+                transform: 'scaleY(0)',
+            };
+
+            //Background muda para tela light
+            dinamicBg = {
+                color: '#222222',
+                backgroundColor: '#f8f8f7'
+            }
         }
 
         return(
             <div>
-                <button onClick = {() => {this.show()}} style={btnQuestion}>
-                <div className="d-flex justify-content-between">
-                    <div className="">
-                        <p className="badge badge-danger">Question</p>
+                <button onClick = {() => {this.show()}} className='btnQuestion' style={dinamicBg}>
+                    <div className="d-flex justify-content-between btnQuestionText">
+                        <div className="">
+                            <p className="badge badge-danger">Question</p>
+                        </div>
+                        <div className="questionText">
+                            <p>
+                                {this.props.texts.quest}
+                            </p>
+                        </div>
+                        <span className='statusQuestion'>
+                            {textAction}
+                        </span>   
                     </div>
-                    <div className="">
-                        <p>
-                            {this.props.texts.quest}
-                        </p>
-                    </div>
-                    <span>
-                        Mostrar
-                    </span>   
-                </div>
                 </button>
-
-                {answer}
+                <div className='answerAnimation' style={animation}>{answer}</div>
+                
                 
             </div>
         )
@@ -46,13 +93,3 @@ class Question extends Component {
 }
 
 export default Question;
-
-const btnQuestion = {
-    width: "100%",
-    textAlign: 'center',
-    textDecoration: 'none',
-    backgroundColor: '#f8f8f7',
-    border: 'none',
-    boxShadow: '0 1px 1px 0 rgba(0,0,0,0.2), 0 1px 1px 0 rgba(0,0,0,0.19)'
-}
-
