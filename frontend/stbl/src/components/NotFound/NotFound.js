7c87d416c2ea2dc3f5a3a364db5483a6d0749ba8
FelipeBarros <barrosfelipe.013@gmail.com>
2019-08-30 19:47:03 -0300

    Estilizando NotFound

--- frontend/stbl/src/components/NotFound/NotFound.js
+++ frontend/stbl/src/components/NotFound/NotFound.js
@@ -1,11 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const NotFound = () => {
     return(
-        <div>  
-            <h1>Pagina nao encontrada</h1>
+        <div className="notfound">
+            <h1>Página nao encontrada :(</h1>
             <h2>Sentimos muito pelo incoveniente</h2>
-            <h3>:(</h3>
+            <div>
+                <p>Algumas razões para isso acontecer:</p>
+                <ul>
+                    <li>A página pode ter sido removida;</li>
+                    <li>A página pode estar temporariamente indisponível;</li>
+                    <li>Você pode ter digitado o endereço errado.</li>
+                </ul>
+            </div>
+
+            <p className="button-pri botaovoltarhome">
+                <Link to="/home">Voltar para Página inicial!<span/></Link>
+            </p>
         </div>
     )    
 }
