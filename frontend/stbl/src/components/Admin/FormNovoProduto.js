ff712f6bf254206f6f11121e4351b5b6f69b8238
GustavoM7 <gustavosmarquesf@gmail.com>
2019-10-03 16:56:04 -0300

    adicionando react dropzone

--- frontend/stbl/src/components/Admin/FormNovoProduto.js
+++ frontend/stbl/src/components/Admin/FormNovoProduto.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import { Link } from "react-router-dom";
+import Dropzone from 'react-dropzone';
+import upload from "../Images/upload.svg";
 import './Admin.css';
 
 class FormNovoProduto extends Component {
@@ -20,6 +22,10 @@ class FormNovoProduto extends Component {
         this.setState({
             [e.target.name]: e.target.value
         })
+    }
+
+    imageIput = (img) =>{
+        this.setState({img: img[0]});
     }
 
     //######## Deverá enviar dados de um novo produto para o banco de dados
@@ -48,6 +54,21 @@ class FormNovoProduto extends Component {
 
             //####### Caso ocorra algum erro, usar alert semelhante ao else abaixo
 
+            //####### Caso não ocorra, passar feedback e resetar form
+            alert("Novo produto criado com exito");
+            this.setState({
+                id: "",
+                img: null,
+                titulo: "",
+                marca: "",
+                preco: '',
+                estoque: '',
+                tipoProduto: "",
+                descricao: "",
+                alert: false,
+                errorMsg: "",
+            })
+
         } else {
             console.log("ERRO")
             this.setState({
@@ -67,43 +88,54 @@ class FormNovoProduto extends Component {
                     <div className='admin-form-item'>
                         <label htmlFor="inputIdProduto">ID:</label>
                         <br/>
-                        <input id="inputIdProduto" type="text" placeholder="ID" name="id" onChange={this.atualizarInput}/>
+                        <input id="inputIdProduto" type="text" placeholder="ID" name="id" onChange={this.atualizarInput} value={this.state.id}/>
                     </div>
             
                     <div className='admin-form-item'>
                         <label htmlFor="inputTitulo">TITULO:</label><br/>
-                        <input id="inputTitulo" type="text" placeholder="TTULO" name="titulo" onChange={this.atualizarInput}/>
+                        <input id="inputTitulo" type="text" placeholder="TTULO" name="titulo" value={this.state.titulo} onChange={this.atualizarInput}/>
                     </div>
 
                     <div className='admin-form-item'>
                         <label htmlFor="inputMarca">MARCA:</label><br/>
-                        <input id="inputMarca" type="text" placeholder="MARCA" name="marca" onChange={this.atualizarInput}/>
+                        <input id="inputMarca" type="text" placeholder="MARCA" name="marca" value={this.state.marca} onChange={this.atualizarInput}/>
                     </div>            
 
                     <div className='admin-form-item'>
                         <label htmlFor="inputPreco">PREÇO:</label><br/>
-                        <input id="inputPreco" type="number" placeholder="00,00" name="preco" onChange={this.atualizarInput}/>
+                        <input id="inputPreco" type="number" placeholder="00,00" name="preco" value={this.state.preco} onChange={this.atualizarInput}/>
                     </div>
 
                     <div className='admin-form-item'>
                         <label htmlFor="inputQtd">QUANTIDADE EM ESTOQUE:</label><br/>
-                        <input id="inputQtd" type="number" placeholder="0" name="estoque" onChange={this.atualizarInput}/>
+                        <input id="inputQtd" type="number" placeholder="0" name="estoque" value={this.state.estoque} onChange={this.atualizarInput}/>
                     </div>
 
                     <div className='admin-form-item'>
                         <label htmlFor="inputTipo">TIPO:</label><br/>
-                        <input id="inputTipo" type="text" placeholder="TIPO" name="tipoProduto" onChange={this.atualizarInput}/>
+                        <input id="inputTipo" type="text" placeholder="TIPO" name="tipoProduto" value={this.state.tipoProduto} onChange={this.atualizarInput}/>
                     </div>
 
                     <div className='admin-form-item'>
                         <label htmlFor="inputDesc">DESCRIÇÃO:</label><br/>
-                        <textarea id="inputDesc" type="text" placeholder="DESCRIÇÃO DO PRODUTO" name="descricao" onChange={this.atualizarInput}/>
+                        <textarea id="inputDesc" type="text" placeholder="DESCRIÇÃO DO PRODUTO" name="descricao" value={this.state.descricao} onChange={this.atualizarInput}/>
                     </div>
 
-                    <div className='admin-form-item'>
-                        <label htmlFor="inputImg">IMAGEM:</label>
-                        <input type="file" id="inputImg"/>
+                    <div>
+                     <Dropzone onDrop={acceptedFiles => this.imageIput(acceptedFiles)}>
+                            {({getRootProps, getInputProps}) => (
+                                <section>
+                                <div className="Dropzone-field" {...getRootProps()}>
+                                    <input {...getInputProps()} type="file" accept="image/x-png,image/gif,image/jpeg" onChange={this.atualizarInput} />
+                                    {this.state.img !== null? (<p>{this.state.img.path}</p>) : <img src={upload} className="Upload-icon" alt="upload icon" />}
+                                    <p>Clique aqui ou arraste uma nova imagem</p>
+                                    
+                                </div>
+                                </section>
+                            )}
+                        </Dropzone>     
                     </div>
+
                 </form>
                 <div className='admin-form-item'>
                     <button className='btn-secundaryy' onClick={() =>{this.enviar()}}>Enviar</button>
