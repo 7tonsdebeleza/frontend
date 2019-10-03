4aa6fa47eb944f3c5f2cea1c9c878919eca72cb4
GustavoM7 <gustavosmarquesf@gmail.com>
2019-10-03 00:25:29 -0300

    implementando permanencia de usuário local (admin)

--- frontend/stbl/src/components/Admin/Admin.js
+++ frontend/stbl/src/components/Admin/Admin.js
@@ -15,48 +15,34 @@ class Admin extends Component {
 
     /*
         State:
-            - login: booleano indicando se há usuário logado
+            - adminlogin: guarda objeto de usuário logado
             - interface: inteiro para alterar qual tela será mostrada
                 - 1: tela principal com todas as opções
                 - 2: tela com histórico de compras
                 - 3: tela para adição de novo produto
                 - 4: tela para edição e pesquisa de produtos
                 - 5: tela para edição na página do blog
-            - adminData: guardará dados que podem ser acessados pelo admin quando estiver logado
+            - adminData: guardará dados do bd que podem ser acessados pelo admin quando estiver logado
     */
     state = {
-        adminLogin: false,
+        adminLogin: null,
         interface: 1,
         adminData: null
     }
     
     //Função que será herdada pela coponente login, login para admin diferenciado
     login = (user) => {
-        // ################# Executar API aqui e passar função abaixo como callback
-        this.changeLogin();
-        console.log(user);
+        // ################# Executar API aqui para conectar no bd e passar função abaixo como callback
+        this.setState({adminLogin: user});
+        localStorage.setItem("@stbl/userNome", user.nome);
+        localStorage.setItem("@stbl/userEmail", user.email);
     }
 
     logout = () =>{
-        // ################# Executar API aqui e passar função abaixo como callback
-        this.changeLogin();
-    }
-
-    //Função para recuperação do estado de login no servidor
-    checkServerLogin = () =>{
-        // ################# Executar API aqui e chamar função após a renderização deste componente
-        // ################# Essa função deve ter como callback a mudança do state admin login
-        // ################# Função abaixo para tetes
-        this.setState({
-            adminLogin: false,
-        })
-    }
-
-    //Função para mudanças internas de login e logout
-    changeLogin = () =>{
-        this.setState({
-            adminLogin: this.adminLogin ? false : true
-        })
+        // ################# Executar API aqui para conectar no bd e passar função abaixo como callback
+        this.setState({adminLogin: null});
+        localStorage.removeItem("@stbl/userNome");
+        localStorage.removeItem("@stbl/userEmail");
     }
 
     //Função para mudar componentes da tela, será herdada por outros componentes
@@ -73,20 +59,26 @@ class Admin extends Component {
         })
     }
 
-    componentDidMount(){
-        this.checkServerLogin();
-        this.getAdminData();
+    componentDidMount = () =>{
+        let user = {
+            nome: localStorage.getItem("@stbl/userNome"),
+            email: localStorage.getItem("@stbl/userEmail"),
+        };
+        if(user.nome !== null) this.setState({adminLogin: user})
     }
 
-
     render(){
-        if(!this.state.adminLogin){
+        if(this.state.adminLogin === null){
             return(
                 <div className="login container">
                     <AdminHeader/>
                     <Login login={this.login}/>
+
                     {/* ######## Butão abaixo para testes sem o backend */}
-                    <button onClick={() => this.changeLogin()}>Testes</button>
+                    <button onClick={() => this.login({
+                        email:"teste@teste.com",
+                        nome: "testes",
+                    })}>Testes</button>
                 </div>
             )
             
@@ -95,7 +87,7 @@ class Admin extends Component {
         else if(this.state.interface === 1){
             return(
                 <div className="login container">
-                    <AdminHeader logout={this.logout}/>
+                    <AdminHeader user={this.state.adminLogin} logout={this.logout}/>
                     <AdminInterface callback={this.changeInterface}/>
                 </div>
             )
@@ -105,7 +97,7 @@ class Admin extends Component {
         else if(this.state.interface === 2){
             return(
                 <div className="login container">
-                    <AdminHeader logout={this.logout}/>
+                    <AdminHeader user={this.state.adminLogin} logout={this.logout}/>
                     <ListarConsultas compras={this.state.adminData}/>
                     <p className="btn-secundaryy">
                         <Link to="#" onClick={() => this.changeInterface(1)}> &larr; Retornar</Link>
@@ -118,7 +110,7 @@ class Admin extends Component {
         else if(this.state.interface === 3){
             return(
                 <div className="login container">
-                    <AdminHeader logout={this.logout}/>
+                    <AdminHeader user={this.state.adminLogin} logout={this.logout}/>
                     <FormNovoProduto />
                     <p className="btn-secundaryy">
                         <Link to="#" onClick={() => this.changeInterface(1)}> &larr; Retornar</Link>
@@ -131,7 +123,7 @@ class Admin extends Component {
         else if(this.state.interface === 4){
             return(
                 <div className="login container">
-                    <AdminHeader logout={this.logout}/>
+                    <AdminHeader user={this.state.adminLogin} logout={this.logout}/>
                     <ListaProdutoEditavel list={this.props.produtos} />
                     <p className="btn-secundaryy">
                         <Link to="#" onClick={() => this.changeInterface(1)}> &larr; Retornar</Link>
@@ -144,7 +136,7 @@ class Admin extends Component {
         else if(this.state.interface === 5){
             return(
                 <div className="login container">
-                    <AdminHeader logout={this.logout}/>
+                    <AdminHeader user={this.state.adminLogin} logout={this.logout}/>
                     <BlogEditor public={this.props.publics} />
                     <p className="btn-secundaryy">
                         <Link to="#" onClick={() => this.changeInterface(1)}> &larr; Retornar</Link>
@@ -157,7 +149,7 @@ class Admin extends Component {
         else {
             return(
                 <div className="login container">
-                    <AdminHeader logout={this.logout}/>
+                    <AdminHeader user={this.state.adminLogin} logout={this.logout}/>
                     <NotFound/>
                 </div>
             )
@@ -181,11 +173,14 @@ const AdminHeader = (props) =>{
                     {
                         props.logout ?
                         <p className="btn-secundaryy" style={{float: "right", marginTop: "10px"}}>
-                            <Link to="#" onClick={() => this.props.logout()}> &larr; Desconectar</Link>
+                            <button onClick={() => props.logout()}> &larr; Desconectar</button>
                         </p> : null
                     }
                 </header>
-                <p className="title">Área exclusiva para administração da loja virtual 7 Tons de Beleza.</p>
+                <p className="title">
+                    {props.user ? ("Bem vindo " + props.user.nome + "! ") : null}
+                    Área exclusiva para administração da loja virtual 7 Tons de Beleza.
+                </p>
                 
             </div>
         </div>
