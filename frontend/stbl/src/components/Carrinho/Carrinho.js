f3948ebd42438873e43d9efc1cb08125030ed121
GustavoM7 <gustavosmarquesf@gmail.com>
2020-01-26 21:59:14 -0300

    melhorando indentação

--- frontend/stbl/src/components/Carrinho/Carrinho.js
+++ frontend/stbl/src/components/Carrinho/Carrinho.js
@@ -5,78 +5,79 @@ import { Link } from "react-router-dom";
 //Usando ListaProdutos provisoamente para mostrar produtos adicionados ao carrinho.
 
 class Carrinho extends Component {
-    state = {
-        naoestalogado: false
-    }
+  state = {
+    naoestalogado: false
+  }
 
-    fecharCompra = () => {
-        this.setState({
-            naoestalogado: true
-        })
-    }
+  fecharCompra = () => {
+    this.setState({
+      naoestalogado: true
+    })
+  }
 
-    render() {
-        let retorno;
-        let subtotal = 0;
+  render() {
+    let retorno;
+    let subtotal = 0;
 
-        if (this.props.dados.length === 0) {
-            retorno = (
-                <div className="container defaultCarrinho">
-                    <div>
-                        Nenhum produto no Carrinho
-                    </div>
-                    <img src={Cart} width='50' height='50' alt='cart'></img>
-                </div>
-            );
-        } else {
-            retorno = <ListaProduto noCarrinho list={this.props.dados} atualizarQtdCarrinho={this.props.atualizarQtdCarrinho} removerCarrinho={this.props.removerCarrinho} naNavbar={this.props.naNavbar}/>
+    if (this.props.dados.length === 0) {
+      retorno = (
+        <div className="container defaultCarrinho">
+          <div> Nenhum produto no Carrinho </div>
+            <img src={Cart} width='50' height='50' alt='cart'></img>
+        </div>
+      );
+    } else {
+      retorno = <ListaProduto noCarrinho list={this.props.dados} atualizarQtdCarrinho={this.props.atualizarQtdCarrinho} removerCarrinho={this.props.removerCarrinho} naNavbar={this.props.naNavbar}/>;
 
-            retorno.props.list.forEach(element => {
-                subtotal = subtotal + element.preco * element.qtd;
-            });
+      retorno.props.list.forEach(element => {
+        subtotal = subtotal + element.preco * element.qtd;
+      });
 
-        } 
+    } 
 
 
-        return (
-            <div className="container">
-                <div className="tituloCarrinho">
-                    <span>PRODUTOS NO CARRINHO:</span>
-                </div>
+    return (
+      <div className="container">
+        <div className="tituloCarrinho">
+          <span>PRODUTOS NO CARRINHO:</span>
+        </div> 
+        
+        { retorno }
+              
+        {
+          subtotal !== 0 ?
+            <div className="subtotalQuantidade">
+              <b>SubTotal: R${ parseFloat((subtotal).toFixed(2))}</b>
+
+              <div className="nav botoesCarrinho">
                 {
-                    retorno
-                }
-                {
-                    subtotal !== 0 ?
-                        <div className="subtotalQuantidade"><b>SubTotal: R${
-                            parseFloat((subtotal).toFixed(2))
-                        }</b>
-                            <div className="nav botoesCarrinho">
-                                {this.props.botaoCarrinho ?
-                                    <p className="nav-item button-sec botao1">
-                                        <Link to="/carrinho" className="btn-secondary">Ir para o Carrinho</Link>
-                                    </p> :
-                                    <p className="nav-item button-sec botao1">
-                                        <Link to="/lojavirtual" className="btn-secondary">Escolher mais produtos</Link>
-                                    </p>
-                                }
+                  this.props.botaoCarrinho ?
+                    <p className="nav-item button-sec botao1">
+                      <Link to="/carrinho" className="btn-secondary">Ir para o Carrinho</Link>
+                    </p> :
 
-                                <p className="nav-item button-pri botao2">
-                                    <Link to="#" onClick={() => { this.fecharCompra() }}>Fechar compra</Link>
-                                </p>
-                                {this.state.naoestalogado ?
-                                    <div className="container" style={{ fontSize: '15px' }}>
-                                        <p>Para finalizar a compra faça Login em sua conta! <Link to="/login">Login</Link></p>
-                                        <p>Ainda não tem uma conta? <Link to="/cadastro">Cadastre-se</Link></p>
-                                    </div> : ""}
-                            </div>
-                        </div> : ""
+                    <p className="nav-item button-sec botao1">
+                      <Link to="/lojavirtual" className="btn-secondary">Escolher mais produtos</Link>
+                    </p>
                 }
 
+                <p className="nav-item button-pri botao2">
+                  <Link to="#" onClick={() => { this.fecharCompra() }}>Fechar compra</Link>
+                </p>
 
-            </div>
-        )
-    }
+                {
+                  this.state.naoestalogado ?
+                    <div className="container" style={{ fontSize: '15px' }}>
+                      <p>Para finalizar a compra faça Login em sua conta! <Link to="/login">Login</Link></p>
+                      <p>Ainda não tem uma conta? <Link to="/cadastro">Cadastre-se</Link></p>
+                    </div> : null
+                }
+              </div>
+            </div> : null
+        }
+      </div>
+    )
+  }
 }
 
 export default Carrinho;
--- frontend/stbl/src/components/Roteador/Roteador.js
+++ frontend/stbl/src/components/Roteador/Roteador.js
@@ -25,246 +25,235 @@ import Publicacao from "../Blog/Publicacao";
 import ErroInterface from "./ErroInterface";
 import api from "../API/api";
 
+// Este componente serve como raiz da aplicação
+// Contendo as rotas e funções de fluxo de dados
+
 class Roteador extends Component {
-	//Este componente é usado como root para o fluxo de dados entre os demais componentes.
-	/*
-	 state:
-		 - "email" guarda o usuário q está logado. [não funcional]
-		 - "dadosCarrinho" guarda uma array de objetos de produtos no carrinho
-		 - "qtdCarrinho" mostra o total de produtos no carrinho, mostrando no contador da navbar
-			 obs.: a qtd individual de cada produto é guardada em cada objeto.
-		 - "pesquisa" guarda a string a ser passada para página de pesquisa
-		 - "pesquisaChamada" ativa a página de resultados de pesquisa
-		 - "dados" guarda a resquisão de produtos do banco de dados
-		 - "carregado" indica se a requisição do banco de dados foi finalizada para renderização
-		 - "user" guarda um objeto de usuário (cliente)
-		 - "error" quando true, retorna tela de erro
-	*/
+  state = {
+	dadosCarrinho: [],
+	qtdCarrinho: 0,
+	pesquisa: "",
+	pesquisaChamada: false,
+	dados: [],
+	carregado: false,
+	user: null,
+	error: false,
+  }
 
-	state = {
-		email: "p",
-		dadosCarrinho: [],
-		qtdCarrinho: 0,
-		pesquisa: "",
-		pesquisaChamada: false,
-		dados: [],
-		carregado: false,
-		user: null,
-		error: false,
-	}
+  //Função que será herdada pelo componente de login do cliente
+  //Recebe como parâmetro dados válidos do form de login
+  clientLogin = async (user) => {
 
-
-	//Função que será herdada pelo componente de login do cliente
-	//Recebe como parâmetro dados válidos do form de login
-	clientLogin = async (user) => {
-
-		console.log("Gerando token...")
-		const res = await api.post('/Sign',{
-			email: user.email,
-			senha: user.senha
-		  });		
+  	console.log("Gerando token...");
+  	const res = await api.post('/Sign',{
+	  email: user.email,
+	  senha: user.senha
+  	});		
         
-		//Caso login não funcione
-		if(res.data.error) return res.data.error
-        if(res.data === "Email inválido!" || res.data === "Senha inválida!"){
-            return res.data;
-        } else {
+  	//Caso login não funcione
+  	if(res.data.error) return res.data.error;
 
-            //Salvando token para permância de login
-            localStorage.setItem('@stbl/client/user', res.data.token);
-			this.auth(res.data.token);
+  	if(res.data === "Email inválido!" || res.data === "Senha inválida!"){
+	  return res.data;
+	
+  	} else {
+	  //Salvando token para permância de login
+      localStorage.setItem('@stbl/client/user', res.data.token);
+	  this.auth(res.data.token);
+	  return true;
 
-            return true;
-		}
-	}
+  	}
+  }
 
-	//Função de autenticação de token
-	auth = (token) => {
-		console.log("Autenticando...")
-		api.post('/Auth',  {headers: {"Authorization" : token}}).then(res => {
-			if(res.data.error){
-				//Caso autenticação falhe, usuário será deslogado para gerar novo token
-				console.log(res.data.error)
-				this.Clientelogout();
-			} else {
-				//Salvando dados do usuário
-				this.setState({user:res.data})
-			}
-		})
-	}
+  //Função de autenticação de token
+  auth = (token) => {
+	console.log("Autenticando...");
+
+	api.post('/Auth',  {headers: {"Authorization" : token}}).then(res => {
+	  if(res.data.error){
+		//Caso autenticação falhe, usuário será deslogado para gerar novo token
+		console.log(res.data.error);
+		this.Clientelogout();
+
+	  } else {
+		//Salvando dados do usuário
+		this.setState({user:res.data});
+		console.log(res.data);
+	  }
+	})
+  }
 	
 
-	Clientelogout = () =>{
-        // Removendo objeto do state e permanência do navegador
-        this.setState({user: null});
-        localStorage.removeItem("@stbl/client/user");
-    }
+  Clientelogout = () =>{
+    // Removendo objeto do state e permanência do navegador
+    this.setState({user: null});
+    localStorage.removeItem("@stbl/client/user");
+  }
 
-
-	//Função que executava a requisição com o banco de dados
-	async mostra(){
-		api.get('/mostrartodosprodutos').then(response => {
-			response.data.map((obj)=>{
-				//Remove o path da imagem e seta como o link dela
-				obj.img = obj.img_url
-				return true
-			})
-			this.setState({
-				dados: response.data,
-				carregado: true,
-			})
-		}).catch(e => {
-			console.log(e);
-			this.setState({error: true});
-		})
-		
-	}
-
-	//Esta função será passada aos componetes filhos onde houver componete produto
-	addCarrinho = (dados) =>{
-		let novaLista = this.state.dadosCarrinho;
-		novaLista.push(dados);
-		this.setState({
-			dadosCarrinho: novaLista,
-			qtdCarrinho: this.state.qtdCarrinho + dados.qtd
-		})
-		
-	}
-
-	atualizarQtdCarrinho = (qtd) =>{
-		this.setState({
-			qtdCarrinho: this.state.qtdCarrinho + qtd
-		})
-	}
-
-	removerCarrinho = (dados) =>{
-		dados.noCarrinho = false;
-		let newArray = this.state.dadosCarrinho;
-		
-		newArray.forEach((element, n) => {
-			if(element.id === dados.id){
-				newArray.splice(n, 1)
-			}
-		});
-
-		this.setState({
-			dadosCarrinho: newArray
-		})
-
-		this.atualizarQtdCarrinho(dados.qtd*(-1));
-	}
-
-	pesquisar = (string) =>{
-		//State recebe string de pesquisa e pesquisa é setada verdadeira
-
-		this.setState({
-		  pesquisa: string,
-		  pesquisaChamada: true, 
-		});
-	}
-
-	componentWillMount(){
-		//Fazendo verificações do endereço
-		let adress = window.location.pathname;
-
-		//Caso endereço seja de página de busca
-		if(adress.includes("/buscar/busca=")){
-			this.setState({
-				pesquisaChamada: true,
-				pesquisa: adress.replace("/buscar/busca=", "").toString()
-			});
-		}
-
-	}
-
-	componentDidMount(){
-		//Carregando produtos do banco de dados 
-		this.mostra();
-
-		//recuperando estado de usuário logado
-		let token = localStorage.getItem("@stbl/client/user");
-        if(token !== null){
-
-			//##### Implementar função para conectar carrinho do cliente logado           
-            
-            this.auth(token)
-        }
-	}
-
-	render() {
-		//Caso haja erro na api
-		if(this.state.error) return <ErroInterface />
-
-		//Os componentes são renderizados apenas se requisião ao banco de dados tiver finalizado
-		if(!this.state.carregado) return <Carregamento />
-		else {
-			return (
-			<BrowserRouter>
+  //Função que executava a requisição com o banco de dados
+  async mostra(){
+	api.get('/mostrartodosprodutos').then(response => {
+	  response.data.map((obj)=>{
+		//Remove o path da imagem e seta como o link dela
+		obj.img = obj.img_url;
+		return true
+	  });
 			
-			<div>
-				<GoTop />
-				<div style={{
-					display:'flex',
-					flex: 1,
-					minHeight: '100vh',
-					flexDirection: 'column',
-				}}>
-					{
-						this.state.pesquisaChamada ? 
-							<Redirect to={'/buscar/busca='+this.state.pesquisa}/>: (null)
-						//Se houver pesquisa, página será redirecionada, depois disso, state é resetada
-					}
+	  this.setState({
+		dados: response.data,
+		carregado: true,
+	  });
 
-					{
-						this.state.pesquisaChamada ? 
-						this.setState({pesquisaChamada: false}) : null
-					}
+	}).catch(e => {
+	  console.log(e);
+	  this.setState({error: true});
+	});
+		
+  }
 
-					<NavBar pesquisar={this.pesquisar} qtdCarrinho={this.state.qtdCarrinho} dados={this.state.dadosCarrinho} atualizarQtdCarrinho={this.atualizarQtdCarrinho} removerCarrinho={this.removerCarrinho} botaoCarrinho={true} user={this.state.user} logout={this.Clientelogout} />
+  //Esta função será passada aos componetes filhos onde houver componete produto
+  addCarrinho = (dados) =>{
+	console.log(dados);
+	let novaLista = this.state.dadosCarrinho;
+	novaLista.push(dados);
 
-					<NavBarMobile pesquisar={this.pesquisar} qtdCarrinho={this.state.qtdCarrinho} dados={this.state.dadosCarrinho} atualizarQtdCarrinho={this.atualizarQtdCarrinho} removerCarrinho={this.removerCarrinho} botaoCarrinho={true} user={this.state.user} logout={this.Clientelogout}/>
+	this.setState({
+	  dadosCarrinho: novaLista,
+	  qtdCarrinho: this.state.qtdCarrinho + dados.qtd
+	});
+		
+  }
+
+  atualizarQtdCarrinho = (qtd) =>{
+	this.setState({
+	  qtdCarrinho: this.state.qtdCarrinho + qtd
+	})
+  }
+
+  removerCarrinho = (dados) =>{
+	console.log(dados);
+	dados.noCarrinho = false;
+	let newArray = this.state.dadosCarrinho;
+		
+	newArray.forEach((element, n) => {
+	  if(element.id === dados.id){
+	  	newArray.splice(n, 1)
+	  }
+	});
+
+	this.setState({
+	  dadosCarrinho: newArray
+	});
+
+	this.atualizarQtdCarrinho(dados.qtd*(-1));
+  }
+
+  pesquisar = (string) =>{
+	//State recebe string de pesquisa e pesquisa é setada verdadeira
+
+	this.setState({
+	  pesquisa: string,
+	  pesquisaChamada: true, 
+	});
+  }
+
+  componentWillMount(){
+	//Fazendo verificações do endereço
+	let adress = window.location.pathname;
+
+	//Caso endereço seja de página de busca
+	if(adress.includes("/buscar/busca=")){
+	  this.setState({
+		pesquisaChamada: true,
+		pesquisa: adress.replace("/buscar/busca=", "").toString()
+	  });
+	}
+
+  }
+
+  componentDidMount(){
+	//Carregando produtos do banco de dados 
+	this.mostra();
+
+	//recuperando estado de usuário logado
+	let token = localStorage.getItem("@stbl/client/user");
+    if(token !== null){
+
+	//##### Implementar função para conectar carrinho do cliente logado             
+    this.auth(token)
+    }
+  }
+
+  render() {
+	//Caso haja erro na api
+	if(this.state.error) return <ErroInterface />;
+
+	//Os componentes são renderizados apenas se requisião ao banco de dados tiver finalizado
+	if(!this.state.carregado) return <Carregamento />;
+
+	else {
+	  return (
+		<BrowserRouter>	
+		  <div>
+			<GoTop />
+
+			<div style={{
+			  display:'flex',
+			  flex: 1,
+			  minHeight: '100vh',
+			  flexDirection: 'column',
+			}}>
+			  { this.state.pesquisaChamada ? <Redirect to={'/buscar/busca='+this.state.pesquisa}/>: (null)
+				//Se houver pesquisa, página será redirecionada, depois disso, state é resetada
+			  }
+
+			  { this.state.pesquisaChamada ? this.setState({pesquisaChamada: false}) : null }
+
+			  <NavBar pesquisar={this.pesquisar} qtdCarrinho={this.state.qtdCarrinho} dados={this.state.dadosCarrinho} atualizarQtdCarrinho={this.atualizarQtdCarrinho} removerCarrinho={this.removerCarrinho} botaoCarrinho={true} user={this.state.user} logout={this.Clientelogout} />
+
+			  <NavBarMobile pesquisar={this.pesquisar} qtdCarrinho={this.state.qtdCarrinho} dados={this.state.dadosCarrinho} atualizarQtdCarrinho={this.atualizarQtdCarrinho} removerCarrinho={this.removerCarrinho} botaoCarrinho={true} user={this.state.user} logout={this.Clientelogout}/>
 					
-					<Switch>
-						<Route exact path="/home" render={() => <Home dados={this.state.dados} addCarrinho={this.addCarrinho} atualizarQtdCarrinho={this.atualizarQtdCarrinho} removerCarrinho={this.removerCarrinho}/>}/>
+			  <Switch>
+				<Route exact path="/home" render={() => <Home dados={this.state.dados} addCarrinho={this.addCarrinho} atualizarQtdCarrinho={this.atualizarQtdCarrinho} removerCarrinho={this.removerCarrinho}/>}/>
 
-						<Route exact path="/cadastro" render={() => this.state.user ? <Redirect to='/Cliente'/> : <Cadastro/>}/>
+				<Route exact path="/cadastro" render={() => this.state.user ? <Redirect to='/Cliente'/> : <Cadastro/>}/>
 
-						<Route exact path="/login" render={() => this.state.user ? <Redirect to='/Cliente'/> : <Login login={this.clientLogin}/>} />
+				<Route exact path="/login" render={() => this.state.user ? <Redirect to='/Cliente'/> : <Login login={this.clientLogin}/>} />
 
-						<Route exact path="/lojavirtual" render={() => <LojaVirtual dados={this.state.dados} addCarrinho={this.addCarrinho} atualizarQtdCarrinho={this.atualizarQtdCarrinho} removerCarrinho={this.removerCarrinho}/>}/>
+				<Route exact path="/lojavirtual" render={() => <LojaVirtual dados={this.state.dados} addCarrinho={this.addCarrinho} atualizarQtdCarrinho={this.atualizarQtdCarrinho} removerCarrinho={this.removerCarrinho}/>}/>
 
-						<Route exact path="/marcas" component={Marcas}/>
+				<Route exact path="/marcas" component={Marcas}/>
 
-						<Route exact path="/faq" component={Faq}/>
+				<Route exact path="/faq" component={Faq}/>
 
-						<Route path="/buscar" component={() => this.state.pesquisa === "" ? <Redirect to='/home'/>:<Busca dados={this.state.dados} pesquisa={this.state.pesquisa} addCarrinho={this.addCarrinho} atualizarQtdCarrinho={this.atualizarQtdCarrinho} removerCarrinho={this.removerCarrinho}/>}/>
+				<Route path="/buscar" component={() => this.state.pesquisa === "" ? <Redirect to='/home'/>:<Busca dados={this.state.dados} pesquisa={this.state.pesquisa} addCarrinho={this.addCarrinho} atualizarQtdCarrinho={this.atualizarQtdCarrinho} removerCarrinho={this.removerCarrinho}/>}/>
 
-						<Route exact path="/carrinho" render={() => <Carrinho dados={this.state.dadosCarrinho} atualizarQtdCarrinho={this.atualizarQtdCarrinho} removerCarrinho={this.removerCarrinho} botaoCarrinho={false} naNavbar={false}/>}/>
+				<Route exact path="/carrinho" render={() => <Carrinho dados={this.state.dadosCarrinho} atualizarQtdCarrinho={this.atualizarQtdCarrinho} removerCarrinho={this.removerCarrinho} botaoCarrinho={false} naNavbar={false}/>}/>
 
-						<Route path="/admin7tons" render={() => <Admin produtos={this.state.dados} consultas={Compras} publics={Public}/>}/>
+				<Route path="/admin7tons" render={() => <Admin produtos={this.state.dados} consultas={Compras} publics={Public}/>}/>
 
-						<Route exact path="/"  render={() => <Home dados={this.state.dados} addCarrinho={this.addCarrinho} atualizarQtdCarrinho={this.atualizarQtdCarrinho} removerCarrinho={this.removerCarrinho} />}/>
+				<Route exact path="/"  render={() => <Home dados={this.state.dados} addCarrinho={this.addCarrinho} atualizarQtdCarrinho={this.atualizarQtdCarrinho} removerCarrinho={this.removerCarrinho} />}/>
 
-						<Route exact path="/cliente" render={() => this.state.user? <Cliente user={this.state.user}/> : <Redirect to="/login"/> }/>
+				<Route exact path="/cliente" render={() => this.state.user? <Cliente user={this.state.user}/> : <Redirect to="/login"/> }/>
 
-						<Route exact path="/cliente/atualizar" render={() => this.state.user? <AtualizarClienteInfo user={this.state.user}/> : <Redirect to="/login"/> }/>
+				<Route exact path="/cliente/atualizar" render={() => this.state.user? <AtualizarClienteInfo user={this.state.user}/> : <Redirect to="/login"/> }/>
 
-						{/*Rotas para publicações na aba blog*/}
+				{/*Rotas para publicações na aba blog*/}
 
-						<Route exact path="/blog" component={() => <Blog publics={Public}><PostsList publics={Public}/></Blog> } /> 
-                		<Route path="/blog/posts/" component={() => <Blog publics={Public}><Publicacao publics={Public}/></Blog> } />
+				<Route exact path="/blog" component={() => <Blog publics={Public}><PostsList publics={Public}/></Blog> } /> 
+                <Route path="/blog/posts/" component={() => <Blog publics={Public}><Publicacao publics={Public}/></Blog> } />
 
-						<Route component={NotFound}/>    			
-					</Switch>
+				<Route component={NotFound}/>    			
+			  </Switch>
 
-					<BotaoTop/>
+			  <BotaoTop/>
 					
-				</div>
-				<Footer/>
-				</div>
-			</BrowserRouter>
-			);
-		}
+			</div>
+			<Footer/>
+		  </div>
+		</BrowserRouter>
+	  );
+	}
 		
   }
 }
