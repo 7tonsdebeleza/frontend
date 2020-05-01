317726a37921a1d7de8c49005609785cb5ccbb49
stormsamurai1 <stormsamurai1@yahoo.com.br>
2020-05-01 17:12:12 -0300

    Adicionando opções de trocar senha e reverter a troca no email

--- backend/controller/EmailController.js
+++ backend/controller/EmailController.js
@@ -29,5 +29,31 @@ module.exports={
 
             return res.send(e)
         }
+    },
+
+    async TrocarSenha(req,res){
+        const { id, email } = req.body
+
+        const msg = {
+            to: `${email}`,
+            from: 'pvocufc@gmail.com',
+            subject: 'Trocar senha',
+            text: 'Clique para trocar sua senha',
+            html: `<a href="http://localhost:3333/trocarsenha/${id}">Clique para trocar a sua senha</a>, se não foi você, <a href="http://localhost:3333/revertersenha/:id">Clique aqui</a> e reverta a troca de senha`,
+          };
+        try{
+            const x = await SendGridMail.send(msg);
+            
+            return res.json({'status': 200})
+        }catch(e){
+            console.log(`[Error]: ${e}`)
+
+            if(e.response){
+                console.log(`[Error response]: ${e.response}`)
+            }
+
+            return res.send(e)
+        }
+        
     }
 }
