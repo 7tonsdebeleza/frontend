8577407477beb10f009086761938f4192574153d
Gustavo Marques <gustavosmarquesf@gmail.com>
2020-09-30 13:00:42 -0300

    fix (frontend): revomendo erro de execução no heroku (scripts npm)

--- frontend/server.js
+++ frontend/server.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const port = process.env.PORT|| 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors())
 app.use(express.static(path.join(__dirname, 'build')));
