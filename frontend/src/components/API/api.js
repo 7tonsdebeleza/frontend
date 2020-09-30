cfb38aca8d44752152bcab03ccb36de59fd2fe70
Gustavo Marques <gustavosmarquesf@gmail.com>
2020-09-29 23:15:28 -0300

    chore (frontend): condigurando url da api publicada na heroku

--- frontend/src/components/API/api.js
+++ frontend/src/components/API/api.js
@@ -7,7 +7,7 @@ export function convertDate(date) {
     return `${time.getDate()} de ${meses[time.getMonth()]}, ${time.getFullYear()}`
 }
 
-const baseURL = process.env.NODE_ENV === 'development' ? `http://localhost:3333` : process.env.API_URL;
+const baseURL = process.env.NODE_ENV === 'development' ? `http://localhost:3333` : 'https://setetonsaapiteste.herokuapp.com';
 const api = axios.create({ baseURL });
 
 export default api;
